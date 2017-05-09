2cf6fd3fed0d7e54f639e8b09b222a03eeb498b3
Anirudh Loka <ALoka@parisartlabel.com>
2017-05-09 15:48:32 -0400

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanapp',
+  //database: 'mongodb://localhost:27017/meanapp',
+  database: 'mongodb://anirudh.developer1:123456@ds137749.mlab.com:37749/meanauthapp',
   secret: 'yoursecret'
 }
